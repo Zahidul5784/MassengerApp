05ec78e22b01d5fbd7135f1d54f5cfcf37e70935
zahidul5784@gmail.com <zahidul5784@gmail.com>
2024-02-05 19:04:41 +0600

    editAdd project files.

---
+++ MassengerApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassengerApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task Share(string user, string message)
+        {
+            await Clients.Caller.SendAsync("receive", "(SELF)", message);
+            await Clients.Others.SendAsync("receive", user, message);
+        }
+    }
+}
---
+++ MassengerApp/wwwroot/js/chat.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../lib/microsoft/signalr/dist/browser/signalr.js" />
+
+
+var username = prompt("enter your name", "DITC");
+
+var hubBuilder = new signalR.HubConnectionBuilder();
+var connection = hubBuilder.withUrl("/chat").build();
+
+
+
+
+
+connection.on("receive", function (user, message) {
+    var usm = document.createElement("abc");
+
+
+    usm.textContent = `${user} : ${message}`;
+
+    $('#chatLog').append(usm);
+
+
+});
+
+connection.start();
+
+
+
+$('#btnSend').on('click', function () {
+
+    var message = $('#txtInput').val();
+
+    connection.invoke("Share", username, message);
+    $('#txtInput').val(null);
+})
